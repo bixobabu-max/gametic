e677b06f9e0f94f329debf08fc53f59405ced788
bixobabu-max <bixobabu@gmail.com>
2025-12-29 14:22:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+const gameBoard = document.getElementById('game-board');
+const statusDisplay = document.getElementById('status');
+const restartButton = document.getElementById('restart-button');
+
+let currentPlayer = 'X';
+let gameActive = true;
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+
+    handleResultValidation();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = `Player ${currentPlayer} has won!`;
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes('');
+    if (roundDraw) {
+        statusDisplay.innerHTML = `Game ended in a draw!`;
+        gameActive = false;
+        return;
+    }
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState = ['', '', '', '', '', '', '', '', ''];
+    statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '');
+}
+
+for (let i = 0; i < 9; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.setAttribute('data-cell-index', i);
+    cell.addEventListener('click', handleCellClick);
+    gameBoard.appendChild(cell);
+}
+
+statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
+restartButton.addEventListener('click', handleRestartGame);
